0153c62cdf4f2abf9dab6f219084f363c8b851a8
kungbi <kipper12335@gmail.com>
2024-04-10 13:35:02 +0900

    first

---
+++ src/main/java/com/toklin/spring_postgres_cicd_demo/SpringPostgresCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.toklin.spring_postgres_cicd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresCicdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostgresCicdDemoApplication.class, args);
+    }
+
+}
