e74ed52fdf67ffca25e5f84acea41747d79308af
kungbi <kipper12335@gmail.com>
2024-04-10 16:04:37 +0900

    spring setting

---
+++ src/main/java/com/toklin/spring_postgres_cicd_demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.toklin.spring_postgres_cicd_demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+
+    @GetMapping()
+    public String getHello() {
+        return "Hello World!";
+    }
+
+}
